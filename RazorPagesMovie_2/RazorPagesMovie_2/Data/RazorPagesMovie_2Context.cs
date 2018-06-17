862705b137ded56d29d98b8cbba0ae3dc35ffc10
Jerry_Bao <jerrybao1998@outlook.com>
2018-06-17 14:55:30 -0600

    Added new movie class and scaffold

---
+++ RazorPagesMovie_2/RazorPagesMovie_2/Data/RazorPagesMovie_2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie_2.Models
+{
+    public class RazorPagesMovie_2Context : DbContext
+    {
+        public RazorPagesMovie_2Context (DbContextOptions<RazorPagesMovie_2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
+    }
+}
--- RazorPagesMovie_2/RazorPagesMovie_2/Startup.cs
+++ RazorPagesMovie_2/RazorPagesMovie_2/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie_2.Models;
 
 namespace RazorPagesMovie_2
 {
@@ -33,6 +35,9 @@ namespace RazorPagesMovie_2
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<RazorPagesMovie_2Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("RazorPagesMovie_2Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
